d46ff571dabf5e92bf92ef47b46678b2a388f9fb
vsoeiu <adrianbarron352@gmail.com>
2025-11-27 12:01:01 -0600

    Version 8

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# $PYTHON_CMD apunta al ejecutable de Python instalado por Railway.
-
-# 1. Instala dependencias con el comando de Python
-$PYTHON_CMD -m pip install -r requirements.txt
-
-# 2. Inicia la aplicación usando el comando de Python y gunicorn
-$PYTHON_CMD -m gunicorn app:app
